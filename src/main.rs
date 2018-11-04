aa65bede26412e5e0cd039a3245109374b82d837
Roopeswar Kommalapati <roopeshkom@gmail.com>
2018-11-04 01:34:27 -0400

    Naive fib in rust

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() -> Result<(), std::num::ParseIntError> {
+    loop{
+        println!("Please enter a number below:");
+        let mut inp = String::new();
+        io::stdin().read_line(&mut inp).unwrap();
+
+        let inp: u32 = inp.trim().parse()?;
+        println!("Fibbonacci {} is {}.", inp, fib(inp));
+    }
+}
+
+fn fib(x : u32) -> u32 {
+    match x {
+        0 => 1,
+        1 => 1,
+        n => fib(n-1) + fib(n-2)
+    }
+}
